f715a08b5fdb96a5833d22fe5ab2afc2f701fe69
burak <burak@voys.ai>
2021-06-15 18:13:28 +0300

    Finish url shortener project

---
+++ url-shortener/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	// Parse flags
+	yamlFileName := flag.String("yaml", "mapping.yaml", "yaml file containing redirections")
+	flag.Parse()
+	yaml, err := ioutil.ReadFile(*yamlFileName)
+	if err != nil {
+		panic(err)
+	}
+
+	mux := defaultMux()
+
+	// Build the MapHandler using the mux as the fallback
+	pathsToUrls := map[string]string{
+		"/urlshort-godoc": "https://godoc.org/github.com/gophercises/urlshort",
+		"/yaml-godoc":     "https://godoc.org/gopkg.in/yaml.v2",
+	}
+	mapHandler := MapHandler(pathsToUrls, mux)
+
+	// Build the YAMLHandler using the mapHandler as the fallback
+	yamlHandler, err := YAMLHandler([]byte(yaml), mapHandler)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Println("Starting the server on :8080")
+	http.ListenAndServe(":8080", yamlHandler)
+}
+
+func defaultMux() *http.ServeMux {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", hello)
+	return mux
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello, world!")
+}
